2fab3f3d16a38e561957b043fcc54bed43dfcb67
Alejandro Gómez <alejandro@dialelo.com>
2013-07-15 10:22:00 +0000

    Parse one or more spaces

--- Main.hs
+++ Main.hs
@@ -6,6 +6,12 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+-- |The 'spaces' functions parses and ignores one or more space characters. The
+-- 'spaces' function that comes with Parsec parses zero or more.
+spaces :: Parser ()
+spaces = skipMany1 space
+
+
 -- |The 'readExpr' function takes an input text and tries to parse it.
 readExpr :: String -> String
 readExpr input = case parse symbol "lisp" input of
