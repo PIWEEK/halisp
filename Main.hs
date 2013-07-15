dce6641f80ab18193b17dc3124a0a34dce06bb94
Alejandro Gómez <alejandro@dialelo.com>
2013-07-15 12:31:53 +0000

    Parse lists and display tokens when parsing succeeds

--- Main.hs
+++ Main.hs
@@ -12,6 +12,7 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             deriving (Show)
 
 
 -- |The 'parseString' parses a String 'LispVal'.
@@ -22,7 +23,9 @@ parseString = do
                 strDelim
                 return $ String str
 
+
 strDelim = char '"'
+
 
 -- |A parser that is able to process escaped characters for a string, trimming
 -- the backslash used to escape it.
@@ -34,6 +37,7 @@ escapedChar = do
                     't' -> '\t'
                     'r' -> '\r'
                     _ -> x          -- '\\' and '"'
+
 
 -- |A string with the various characters used as escape codes.
 escapeCharOptions = "\\\"ntr"
@@ -52,9 +56,11 @@ parseAtom = do
                         "#f" -> Bool False
                         _ -> Atom atom
 
+
 -- |The 'parseNumber' parses a Number 'LispVal'.
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
+
 
 -- Exercise 1
 parseNumber' :: Parser LispVal
@@ -74,9 +80,16 @@ parseExpr = parseAtom
         <|> parseNumber''
         <?> "Atom, string or number"
 
+
+-- |The 'parseList' parses a list of expressions.
+parseList :: Parser LispVal
+parseList = parseExpr `sepBy` spaces >>= return . List
+
+
 -- |The 'symbol' function recognizes a symbol allowed in Scheme identifiers.
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
 
 -- |The 'spaces' functions parses and ignores one or more space characters. The
 -- 'spaces' function that comes with Parsec parses zero or more.
@@ -86,9 +99,9 @@ spaces = skipMany1 space
 
 -- |The 'readExpr' function takes an input text and tries to parse it.
 readExpr :: String -> String
-readExpr input = case parse parseString "lisp" input of
+readExpr input = case parse parseList "lisp" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Right val -> "Found value: " ++ show val
 
 
 main :: IO ()
