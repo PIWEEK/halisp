94d371279cdd39b9b83b16dc9d064b17a8aa404e
Alejandro Gómez <alejandro@dialelo.com>
2013-07-15 10:26:26 +0000

    Match one ore more spaces before symbol

--- Main.hs
+++ Main.hs
@@ -14,7 +14,7 @@ spaces = skipMany1 space
 
 -- |The 'readExpr' function takes an input text and tries to parse it.
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
