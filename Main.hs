f204123b4ec496d121672df5ac63d01788cfe596
Alejandro Gómez <alejandro@dialelo.com>
2013-07-15 10:37:40 +0000

    Type for Lisp values

--- Main.hs
+++ Main.hs
@@ -2,6 +2,16 @@ import System.Environment (getArgs)
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+
+-- |The different values that can appear in our Lisp programs.
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal -- represents the Scheme form (a b . c)
+             | Number Integer
+             | String String
+             | Bool Bool
+
+
 -- |The 'symbol' function recognizes a symbol allowed in Scheme identifiers.
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
