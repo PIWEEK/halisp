781e37311d7c75e63952e19fd15f4d9428b251ae
Alejandro Gómez <alejandro@dialelo.com>
2013-07-16 13:00:51 +0000

    Only allow boolean expressions in the if special form

--- src/Core.hs
+++ src/Core.hs
@@ -82,7 +82,9 @@ eval (List [Atom "if", pred, conseq, alt]) =
     do result <- eval pred
        case result of
          Bool False -> eval alt
-         otherwise -> eval conseq
+         Bool True -> eval conseq
+         otherwise -> throwError $ TypeMismatch "Boolean" result
+
 eval (List (Atom func: args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
