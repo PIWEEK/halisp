3edec9efe71824af957988167da977ce47858788
Alejandro Gómez <alejandro@dialelo.com>
2013-07-16 15:38:27 +0000

    Unfinished work on variables

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,65 @@
 import System.IO
 import System.Environment (getArgs)
+
 import Control.Monad (liftM)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Error (runErrorT, throwError, ErrorT)
+
+import Data.IORef
 
 import Parser (readExpr)
-import Core (eval, trapError, extractValue)
+import Core
+
+
+type Env = IORef [(String, IORef LispVal)]
+
+
+-- Environment
+
+nullEnv :: IO Env
+nullEnv = newIORef []
+
+
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef >>= return . maybe False (const True) . lookup var
+
+
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var = do env <- liftIO $ readIORef envRef
+                       maybe (throwError $ UnboundVar "Getting an unbound variable " var)
+                             (liftIO . readIORef)
+                             (lookup var env)
+
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value = do env <- liftIO $ readIORef envRef
+                             maybe (throwError $ UnboundVar "Setting an unbound variable " var)
+                                   (liftIO . (flip writeIORef value))
+                                   (lookup var env)
+                             return value
+
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value = do alreadyDefined <- liftIO $ isBound envRef var
+                                if alreadyDefined
+                                    then setVar envRef var value >> return value
+                                    else liftIO $ do
+                                        valueRef <- newIORef value
+                                        env <- readIORef envRef
+                                        writeIORef envRef ((var, valueRef) : env)
+                                        return value
+
+
+-- IO and error handling
+
+type IOThrowsError = ErrorT LispError IO
+
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+
+runIOThrows :: IOThrowsError String -> IO String
+runIOThrows action = runErrorT (trapError action) >>= return . extractValue
 
 
 -- Helper functions
